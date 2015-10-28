70522a34a2d1a596ac3b4f27551c9cbc225730d4
Stuart Brown <stuartbrown@Stuarts-MacBook-Pro.local>
2015-10-28 15:56:10 +0000

    First commit on new branch

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 /* Redirect browser */
 header("Location: /public/index.html");
+//a quick test to validate the new branch
  
 /* Make sure that code below does not get executed when we redirect. */
 exit;
