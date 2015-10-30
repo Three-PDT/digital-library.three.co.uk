196d28993bdc7b42ec1d4f692dc2055eef19348a
stuartbrown <jinky32@gmail.com>
2015-10-30 14:49:52 +0000

    Refactor of atoms. Inclusion of empty molecule directories

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Redirect browser */
-header("Location: /public/index.html");
-//a quick test to validate the new branch
- 
-/* Make sure that code below does not get executed when we redirect. */
-exit;
-?>
